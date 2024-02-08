240e3a9cb0e59048e38d820bc9de3cfcec6c37fd
leandro-miranda <leandromiranda2010@gmail.com>
2024-02-08 14:39:16 -0300

    redis

--- polls/src/http/routes/vote-on-poll.ts
+++ polls/src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import { randomUUID } from 'node:crypto';
 import { prisma } from '../../lib/prisma';
 import { FastifyInstance } from 'fastify';
+import { redis } from '../../lib/redis';
 
 export async function voteOnPoll(app: FastifyInstance) {
 	app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -37,6 +38,8 @@ export async function voteOnPoll(app: FastifyInstance) {
 						id: userPreviousVoteOnPoll.id,
 					}
 				})
+
+				await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
 			} else if (userPreviousVoteOnPoll) {
 				return reply.status(400).send({ message: 'You already voted on this poll.' });
 			}
@@ -60,6 +63,8 @@ export async function voteOnPoll(app: FastifyInstance) {
 				pollOptionId,
 			}
 		});
+
+		await redis.zincrby(pollId, 1, pollOptionId);
 
 		return reply.status(201).send();
 	})
